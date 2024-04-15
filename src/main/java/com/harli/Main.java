106a60c30d7f093a59973fcab0893ad5de51706a
HarliHaan <harlihaan@gmail.com>
2024-04-15 14:05:43 +0300

    Java Objects to JSON Objects

--- src/main/java/com/harli/Main.java
+++ src/main/java/com/harli/Main.java
@@ -1,9 +1,18 @@
 package com.harli;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 @SpringBootApplication
 
+@RestController
 
 public class Main {
 
@@ -13,4 +22,25 @@ public class Main {
         System.out.println("Hello");
         SpringApplication.run(Main.class, args);
     }
+    @GetMapping("/greet")
+    public GreetResponse greet() {
+        GreetResponse response = new GreetResponse("Hello",
+                List.of("Java", "Golang", "Javascript"),
+                new Person("Alex", 28, 30_000)
+
+        );
+        return response;
+    }
+    record Person (String name, int age, double savings){
+
+    }
+
+    record GreetResponse(String greet,
+                         List<String> favProgrammingLanguages,
+                         Person person
+    ){
+
+    }
+
+
 }
